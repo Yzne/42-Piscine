36ed620c64bc70eaee541d05379f5561e8d76169
yzne <yznsalameh@gmail.com>
2024-09-08 18:48:33 +0400

    C03/ex04

---
+++ C03/ex04/ft_strstr.c
@@ -0,0 +1,38 @@
+
+#include <stdio.h>
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	if (to_find[0] == '\0')
+		return (str);
+	while (str[i] != '\0')
+	{
+		while (to_find[j] != '\0' && str[i + j] == to_find[j])
+		{
+			j++;
+		}
+		if (to_find[j] == '\0')
+			return (&str[i]);
+		i++;
+	}
+	return ('\0');
+}
+/* 
+int	main(void)
+{
+	char str[50] = "I'm dope and I know it!";
+	char to_find[6] = "dope";
+
+	char *yo = ft_strstr(str, to_find);
+	if (yo != NULL)
+		printf("Found: %s\n", yo);
+	else
+		printf("Found Nothing!\n");
+	return (0);
+}
+ */
