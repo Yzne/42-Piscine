948b158d979fe0d692b71eb7da1bf99ef91e5b27
yzne <yznsalameh@gmail.com>
2024-09-09 15:50:09 +0400

    update

--- C04/ex02/ft_putnbr.c
+++ C04/ex02/ft_putnbr.c
@@ -10,21 +10,27 @@ void	ft_putnbr(int nb)
 {
 	char	c;
 
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
 	if (nb < 0)
 	{
 		ft_putchar('-');
 		nb = -nb;
 	}
 	if (nb > 9)
+	{
 		ft_putnbr(nb / 10);
+	}
 	c = (nb % 10) + '0';
 	ft_putchar(c);
 }
-/* 
+
 int	main(void)
 {
-	int	nbr = 420;
+	int	nbr = -2147483648;
 	ft_putnbr(nbr);
 	return (0);
 }
- */
