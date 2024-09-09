4e3c3b8558b0e1bd9367f70b73be4025ac72d426
yzne <yznsalameh@gmail.com>
2024-09-09 12:07:12 +0400

    Update_240909_01

--- C04/ex02/ft_putnbr.c
+++ C04/ex02/ft_putnbr.c
@@ -1,16 +1,30 @@
 
 #include <unistd.h>
 
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
 void	ft_putnbr(int nb)
 {
 	char	c;
 
-	if (nb )
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb > 9)
+		ft_putnbr(nb / 10);
+	c = (nb % 10) + '0';
+	ft_putchar(c);
 }
-
+/* 
 int	main(void)
 {
-	int	nbr = 42;
+	int	nbr = 420;
 	ft_putnbr(nbr);
 	return (0);
 }
+ */
